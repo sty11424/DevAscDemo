e72ba794e9825691f70af20d13434dc82965fe4b
syam <syam@rivierasolutions.net>
2025-10-26 17:15:51 -0400

    Data Serialization - Xmltodict lesson

---
+++ xmlreader.py
@@ -0,0 +1,10 @@
+import xmltodict
+
+# the 'with' statement ensures proper file handling; opening/closing when read is complete
+with open('r1.xml') as file:
+    xml_data = file.read() # read the entire contents of r1.xml and stored in xml_data variable
+
+# use the parse method from xmltodict to convert XML string into a Python dictionary and store in data variable
+data = xmltodict.parse(xml_data)
+
+print(data)
